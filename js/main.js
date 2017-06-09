0a8f326018fbce1b2af07289bdcafa27cddbcd31
Greg Wilwerding <greg.wilwerding@gmail.com>
2017-06-09 12:48:40 -0500

    paddle bounce working

--- js/main.js
+++ js/main.js
@@ -13,30 +13,64 @@ var drawCanvas = function() {
 };
 drawCanvas();
 
-//create ball object with start position, trajectory
+//create and add the paddle to the screen
+var paddle = {
+  location:{},
+  direction: "",
+  init: function(){
+    this.location = {x: 175, y: 380}
+  },
+  draw: function(){
+    ctx.beginPath();
+    ctx.rect((paddle.location.x), (paddle.location.y), 50, 10)
+    ctx.fillStyle = "blue";
+    ctx.fill();
+  },
+  //paddle controls left or right
+  move: function(){
+    if(paddle.direction === "right" && paddle.location.x >= 346) {
+      paddle.location = {x: paddle.location.x, y: 380}; 
+    } else if(paddle.direction === "right" && paddle.location.x <= 346) {
+      paddle.location = {x: paddle.location.x + 2, y: 380};       
+    }else if(paddle.direction === "left" && paddle.location.x <= 4) {
+      paddle.location = {x: paddle.location.x, y: 380}  
+    }else if(paddle.direction === "left" && paddle.location.x >= 4) {
+      paddle.location = {x: paddle.location.x - 2, y: 380}  
+    }
+  }
+}
+//padde controls
+document.addEventListener('keydown', function(event){
+  var key = event.which;
+  if(key === 39){
+    paddle.direction = "right";
+  } else if (key === 37){
+    paddle.direction = "left";
+  } 
+});
+
+//create ball
 var ball = {
   location: {},
-  initBall: function() {
-    this.location = {x: 200, y: 370}
+  radius: 10,
+  init: function() {
+    this.location = {x: 200, y: 369}
   },
   direction: {tx: 2, ty: -2},
 //create function to draw the ball w/10 radius
-  drawBall: function() {
+  draw: function() {
     ctx.beginPath();
-    ctx.arc(ball.location.x, ball.location.y, 10, 0, Math.PI * 2, true);
+    ctx.arc(ball.location.x, ball.location.y, ball.radius, 0, Math.PI * 2, true);
     ctx.closePath();
     ctx.fillStyle = "green";
     ctx.fill();
   },
-  ballMove: function() {
+  move: function() {
     //loop to check if ball is inside the canvas -10 size(ball radius)
-    if (ball.location.y >= canvasSize.width - 10){
-      //change direction of movement
-      //ball.direction.tx = ball.direction.tx; -- add back later once bounce angles are figured out
-      ball.direction.ty = ball.direction.ty - 2;
-      //update ball location, direction
+    if(ball.location.y > 370 && ball.location.x > paddle.location.x && ball.location.x <= paddle.location.x + 50) {
+      ball.direction.ty = ball.direction.ty -2;
       ball.location = {x: ball.location.x + ball.direction.tx, y: ball.location.y + ball.direction.ty};
-    } else if (ball.location.x >= canvasSize.height -10){
+    } else if (ball.location.x >= canvasSize.width -10){
       ball.direction.tx = ball.direction.tx - 2;
       //ball.direction.ty = ball.direction.ty;  -- add back later once bounce angles are figured out
       ball.location = {x: ball.location.x + ball.direction.tx, y: ball.location.y + ball.direction.ty};
@@ -52,58 +86,22 @@ var ball = {
     } else {
       ball.location = {x: ball.location.x + ball.direction.tx, y: ball.location.y + ball.direction.ty};
     }
-  }
+  } 
 }
-
-//create and add the paddle to the screen
-var paddle = {
-  location:{},
-  direction: "",
-  initPaddle: function(){
-    this.location = {x: 175, y: 380}
-  },
-  drawPaddle: function(){
-    ctx.beginPath();
-    ctx.rect((paddle.location.x), (paddle.location.y), 50, 10)
-    ctx.fillStyle = "blue";
-    ctx.fill();
-  },
-  //paddle controls left or right
-  paddleMove: function(){
-    if(paddle.direction === "right") {
-      paddle.location = {x: paddle.location.x + 10, y: 380}; 
-    }else if(paddle.direction === "left") {
-      paddle.location = {x: paddle.location.x - 10, y: 380}  
-    }else {
-    }
-  }
-}
-
-//padde controls
-document.addEventListener('keydown', function(event){
-  var key = event.which;
-  if(key === 39){
-    paddle.direction = "right";
-  }else if(key === 37){
-    paddle.direction = "left";
-  } else {                            //empy else so any button doesn't move left
-  }
-  console.log(paddle.direction);
-});
 
 //add the ball, paddle to the screen
-ball.initBall();
-ball.drawBall();
-paddle.initPaddle();
-paddle.drawPaddle();
+paddle.init();
+paddle.draw();
+ball.init();
+ball.draw();
 
 //animate the canvas
 var animateCanvas = function() {
   ctx.clearRect(0, 0, canvasSize.width, canvasSize.height);
-  ball.ballMove();
-  ball.drawBall();
-  paddle.paddleMove();
-  paddle.drawPaddle();
+  ball.move();
+  ball.draw();
+  paddle.move();
+  paddle.draw();
   window.requestAnimationFrame(animateCanvas);
 }
 
@@ -114,6 +112,10 @@ canvas.addEventListener('click', function() {
 
 //testing reset button to restart the ball at the original location. 
 var reset = document.getElementById('reset').addEventListener('click', function(){
+  ball.direction.tx = 2;
+  ball.direction.ty = -2;
+  ball.location.x = "";
+  ball.location.y = "";
 });
 
 //stop button
